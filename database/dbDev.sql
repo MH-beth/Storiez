d3c7d396eba651e8c39fc1ce5c256b192a92a367
MH15O <76598767+MH15O@users.noreply.github.com>
2021-07-28 15:31:46 +0100

    Delete dbDev.sql

--- database/dbDev.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-    `id` int(11) NOT NULL,
-    `username` varchar(52) NOT NULL,
-    `password` varchar(52) NOT NULL,
-    `email` varchar(52) NOT NULL,
-    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)ENGINE =InnoDB DEFAULT CHARSET=latin1;
